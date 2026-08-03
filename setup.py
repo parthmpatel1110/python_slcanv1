9604820b99e9b170f05f809ce6c1008aa7cb6ce1
parthmpatel1110 <parthmpatel1110@gmail.com>
2026-08-03 13:50:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DynoSure-slcanv1',
-    version='0.4.6',
+    version='0.4.7',
     packages=find_packages(),
     include_package_data=True,
     package_data={'slcanv1': ['*.dll']},
